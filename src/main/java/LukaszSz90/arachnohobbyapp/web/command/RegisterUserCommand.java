b5f555ed460cc16b5fe74e5e20ceb9b6bc73d311
LukaszSz90 <lukasz90sz@gmail.com>
2022-01-31 16:21:47 +0100

    domain-web :: add user and tarantula entities, add registration controller and register user command

---
+++ src/main/java/LukaszSz90/arachnohobbyapp/web/command/RegisterUserCommand.java
@@ -0,0 +1,17 @@
+package LukaszSz90.arachnohobbyapp.web.command;
+
+import lombok.Data;
+import javax.validation.constraints.*;
+
+@Data
+public class RegisterUserCommand {
+
+    @NotNull
+    private String username;
+
+    @NotNull @Email
+    private String email;
+
+    @NotBlank @Size(min = 8, max = 128)
+    private String password;
+}
